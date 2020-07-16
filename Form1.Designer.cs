2abdad02c3895c5a83a78a10cc4cddb2282f7b3a
openbeast90 <61931344+openbeast90@users.noreply.github.com>
2020-07-16 16:03:07 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace _3rewfdsfdsfds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.btnDetect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(12, 89);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(498, 426);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            this.pic.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(12, 62);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(498, 21);
+            this.cboDevice.TabIndex = 1;
+            this.cboDevice.SelectedIndexChanged += new System.EventHandler(this.cboDevice_SelectedIndexChanged);
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(12, 24);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(498, 23);
+            this.btnDetect.TabIndex = 2;
+            this.btnDetect.Text = "detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(517, 527);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.cboDevice);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.Text = "face reconistion";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.Button btnDetect;
+    }
+}
+
